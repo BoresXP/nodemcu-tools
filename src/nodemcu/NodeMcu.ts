1d9ec64fd99612e9750e577f5c23c73beb9e5f38
serg3295 <serg3295@gmail.com>
2023-03-04 13:12:27 +0300

    fix: getChipID command

--- src/nodemcu/NodeMcu.ts
+++ src/nodemcu/NodeMcu.ts
@@ -15,9 +15,9 @@ interface INodeMcuCommandState {
 
 export default class NodeMcu extends NodeMcuSerial implements INodeMcu {
 	private static readonly _luaCommands = {
-		nodeDisableOutput: 'node.output(function(opipe) return false end, 0);uart.write(0, "Done\\r\\n")',
-		nodeEnableOutput: 'node.output();uart.write(0, "Done\\r\\n")',
-		getChipID: 'uart.write(0,tostring(node.chipid()).."\\r\\n");',
+		nodeDisableOutput: 'node.output(function(p) return false end,0);uart.write(0,"Done\\r\\n")',
+		nodeEnableOutput: 'node.output();uart.write(0,"Done\\r\\n")',
+		getChipID: 'uart.write(0,tostring(node.chipid()).."\\r\\n")',
 	}
 	private static readonly _commandTimeout = 15000 // msec
 
