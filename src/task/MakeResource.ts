4bed77fd0fc5181f238bc5827dc6f1eeb45f55c4
serg3295 <serg3295@gmail.com>
2024-06-11 10:18:41 +0300

    refactor: MakeResource

--- src/task/MakeResource.ts
+++ src/task/MakeResource.ts
@@ -13,31 +13,8 @@ export async function makeResource(config: IConfiguration): Promise<Uri | undefi
 		return void 0
 	}
 
-	async function preprocessLua(listOfResourceFiles: string[]): Promise<Buffer | void> {
-		let content = Buffer.from('--  luacheck: max line length 10000\nlocal arg = ...\n')
-		const files = listOfResourceFiles.map(fileName =>
-			workspace.fs.readFile(Uri.joinPath(rootFolder.uri, config.resourceDir, fileName)),
-		)
-
-		const data = await Promise.all(files)
-		for (let i = 0; i < listOfResourceFiles.length; i++) {
-			const fileName = listOfResourceFiles[i]
-			// (if arg == "%s" then return %q end\n\n'):format(fileName, content)
-			const bodyHead = Buffer.from(`if arg == "${fileName}" then return `)
-			const body = emulateLuaQoption(data[i])
-			const bodyTail = Buffer.from(' end\n\n')
-			content = Buffer.concat([content, bodyHead, body, bodyTail])
-		}
-
-		// ('if arg == nil then return {%s} end\n'):format(filelist)
-		const filesList = listOfResourceFiles.map(fn => `"${fn}"`).join(', ')
-		const filesLFS = Buffer.from(`if arg == nil then return {${filesList}} end\n`)
-		content = Buffer.concat([content, filesLFS])
-		return content
-	}
-
 	try {
-		const content = await preprocessLua(fileNames)
+		const content = await preprocessLua(resourceFolder, fileNames)
 		if (content) {
 			await workspace.fs.writeFile(fileToWrite, content)
 			return fileToWrite
@@ -81,6 +58,26 @@ async function getFilesListFromFolder(folder: Uri): Promise<string[]> {
 	}
 }
 
+async function preprocessLua(resourceFolder: Uri, listOfResourceFiles: string[]): Promise<Buffer | void> {
+	let content = Buffer.from('--  luacheck: max line length 10000\nlocal arg = ...\n')
+	const files = listOfResourceFiles.map(fileName => workspace.fs.readFile(Uri.joinPath(resourceFolder, fileName)))
+
+	const data = await Promise.all(files)
+	for (let i = 0; i < listOfResourceFiles.length; i++) {
+		const fileName = listOfResourceFiles[i]
+		// (if arg == "%s" then return %q end\n\n'):format(fileName, content)
+		const bodyHead = Buffer.from(`if arg == "${fileName}" then return `)
+		const body = emulateLuaQoption(data[i])
+		const bodyTail = Buffer.from(' end\n\n')
+		content = Buffer.concat([content, bodyHead, body, bodyTail])
+	}
+
+	// ('if arg == nil then return {%s} end\n'):format(filelist)
+	const filesList = listOfResourceFiles.map(fn => `"${fn}"`).join(', ')
+	const filesLFS = Buffer.from(`if arg == nil then return {${filesList}} end\n`)
+	content = Buffer.concat([content, filesLFS])
+	return content
+}
 
 function emulateLuaQoption(data: Uint8Array): Buffer {
 	let buf: Buffer
@@ -90,9 +87,11 @@ function emulateLuaQoption(data: Uint8Array): Buffer {
 		// It puts a backslash in front of the double quote character (hex 22), backslash itself (hex 5C), and newline (hex 0A).
 		if (s === 0x22 || s === 0x5c || s === 0x0a) {
 			buf = Buffer.from([0x5c, s])
-		} else if ((s >= 0 && s <= 0x1f) || s === 0x7f) { // control characters
+		} else if ((s >= 0 && s <= 0x1f) || s === 0x7f) {
+			// control characters
 			const sStr = s.toString(10)
-			if (!(data[i + 1] >= 0x30 && data[i + 1] <= 0x39)) { // next char is not digit
+			if (!(data[i + 1] >= 0x30 && data[i + 1] <= 0x39)) {
+				// next char is not digit
 				buf = Buffer.from(`\\${sStr}`)
 			} else {
 				buf = Buffer.from('\\000')
